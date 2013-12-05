4f18e33ef736432e9a68bedb73d4b7add23a36bd
Kelly Hutchins <khutchins@esri.com>
2013-12-05 10:50:17 -0800

    initial commit

---
+++ nls/ro/template.js
@@ -0,0 +1,14 @@
+﻿define(
+({
+  app:{
+   title:"Carusel de hărţi",
+    scaleBarUnits: "metric", //"english (for miles) or "metric" (for km) - don't translate.
+    errors:{
+      createMap: "Imposibil de creat harta",
+      bitly: 'O cheie bitly este utilizată pentru scurtarea URL-ului în vederea partajării. Vizualizaţi fişierul readme pentru detalii despre crearea şi utilizarea unei chei bitly',
+      general: "Eroare"
+    },
+    mapcount: "Harta ${page} din ${total}"
+  }
+})
+);
